b047ed4903fa0df616de4b6c7f57458da01d5bfc
HJongMin <hjm@embian.com>
2015-07-10 19:15:34 +0900

    init

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from flask import Flask, render_template
 from flask.ext.socketio import SocketIO
 
+import redis
+import threading
+import time
+
+
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+r = redis.client.StrictRedis()
+
+sub = r.pubsub()
+sub.subscribe("test")
+
+def listen():
+        global temp
+        for m in sub.listen():
+                print m['data']
+                if m['data']:
+                        socketio.emit('msg', m['data'])
+        return
+
+t = threading.Thread(target = listen)
+t.daemon = True
+t.start()
 
 def set_guest():
 	global cnt
@@ -23,17 +45,16 @@ def noname_event(msg):
 
 @socketio.on('msg')
 def msg(msg):
-	socketio.emit('msg', msg)
+	r.publish('test', msg)
+	print msg
+	return	
 
 @app.route('/')
 def index():
 	return render_template('ws_test.html')
-#	return "Hello, world!"
 
 @app.route('/chat')
 def chat():
-#	cnt = cnt + 1	
-#	nick = "Guest_" + cnt
 	nick = set_guest()
 	return render_template('chat.html', nick = nick)
 
@@ -42,6 +63,5 @@ cnt = 0
 if __name__ =='__main__':
 	app.debug=True
 	socketio.run(app)
-
 
 #	app.run(debug = True)
